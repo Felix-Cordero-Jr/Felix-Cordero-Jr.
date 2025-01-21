93b531ef3279fae7349fe5f107b2f91445072661
Felix Cordero Jr. <felix.seniorwebdev@gmail.com>
2025-01-21 21:45:07 +0800

    remove .next js

--- src/app/api/visitor-count/index.ts
+++
@@ -1,4 +0,0 @@
-import { GET as getVisitorCount } from './route';
-import { GET as getNewVisitorCount } from './new-route';
-
-export { getVisitorCount, getNewVisitorCount };
--- src/app/components/VisitorCount.tsx
+++ src/app/components/VisitorCount.tsx
@@ -1,36 +1,11 @@
-// src/app/components/VisitorCount.tsx
-
 import { useState, useEffect } from 'react';
 
 const VisitorCount = () => {
     const [visitorCount, setVisitorCount] = useState<number>(0);
 
-    // This function fetches the visitor data from the API
-    const fetchVisitorData = async () => {
-        try {
-            const response = await fetch('/api/visitor-count', {
-                method: 'GET',
-            });
-
-            // Ensure the response is successful
-            if (!response.ok) {
-                const errorDetails = await response.text();
-                throw new Error(`Failed to fetch data: ${response.statusText} - ${errorDetails}`);
-            }
-
-            const text = await response.text();  // Get the response body as text
-            const data = text ? JSON.parse(text) : { count: 0 };
-
-            // Update the state with the new visitor count
-            setVisitorCount(data.count);
-        } catch (error) {
-            console.error('Error fetching visitor count:', error);
-        }
-    };
-
-    // Fetch visitor data on component mount
+    // Increment the visitor count each time the component is rendered
     useEffect(() => {
-        fetchVisitorData();
+        setVisitorCount((prevCount) => prevCount + 1);
     }, []);
 
     return (
